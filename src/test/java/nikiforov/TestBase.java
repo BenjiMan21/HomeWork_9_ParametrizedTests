7d4b2156c0d63768949b523d0e528b6a51b927df
Alex Nikiforov <unleash211@gmail.com>
2024-01-26 14:00:36 +0400

    change TestBase

--- src/test/java/nikiforov/TestBase.java
+++ src/test/java/nikiforov/TestBase.java
@@ -1,17 +1,22 @@
 package nikiforov;
 
 import com.codeborne.selenide.Configuration;
-import org.junit.jupiter.api.BeforeAll;
+import com.codeborne.selenide.Selenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 
 import static com.codeborne.selenide.Selenide.open;
 
 public class TestBase {
-    @BeforeAll
-    static void beforeAll() {
+    @BeforeEach
+    void beforeEach() {
         open("https://store.steampowered.com/");
-        Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
         //Configuration.holdBrowserOpen = true;//
         Configuration.timeout = 5000; // default 4000
+    }
+    @AfterEach()
+    void afterEach() {
+        Selenide.closeWebDriver();
     }
 }
